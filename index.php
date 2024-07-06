b28c0496e815b8ddb2369a40a514304ee551e341
Triet Le <trietle101@gmail.com>
2024-07-06 11:24:53 +0700

    miscellaneous

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ROBO PET</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/logo.png">
     <link rel="stylesheet" href="assets/css/reset.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
